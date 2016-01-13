c1bf08a1982b1b5235bd227f137066c96de37058
Samantha Fierro <sfierro@mit.edu>
2016-01-12 22:16:58 -0500

    added form submission example

--- vertical-timeline/php-login-script/index.php
+++ vertical-timeline/php-login-script/index.php
@@ -5,5 +5,15 @@ if(!isset($_SESSION['UserData']['Username'])){
 	exit;
 }
 ?>
+<!-- put link to what page it goes to after submit pressed here (action=...)--> 
+<FORM action="http://server.com/cgi/handle" 
+       enctype="multipart/form-data"
+       method="post">
+<P>
+Title <INPUT type="text" name="submit-title"><BR>
+Content <INPUT type="text" name="submit-content"><BR>
+Image/Video <INPUT type="file" name="files"><BR>
+<INPUT type="submit" value="Send"> <INPUT type="reset">
+</FORM>
 
 Congratulations! You have logged into password protected page. <a href="logout.php">Click here</a> to Logout.
