026cf3c0ede4ae67f3afa4c1813c68644386973c
Michelle Lauer <mflauer@mit.edu>
2016-01-20 14:20:01 -0500

    css changes to login page

--- php-login-script/index.php
+++ php-login-script/index.php
@@ -9,16 +9,22 @@ if(!isset($_SESSION['UserData']['Username'])){
 <!-- put link to what page it goes to after submit pressed here (action=...)--> 
 <!-- <FORM action= "http://cajubelt.scripts.mit.edu/code4good-itrek/redirecting.php" -->
 <!--USE THIS TO HOST LOCALLY -->
+<!doctype html>
+<html>
+<head>
+	<link href="./css/style.css" rel="stylesheet">
+</head>
 <FORM action="http://localhost:8888/redirecting.php" 
        enctype="multipart/form-data"
        method="post" style="margin-left:2em">
 <P>
 <br>
-Title <br> 
+<h2>New Timeline Submission</h2>
+<label for="submit-title">Title</label><br> 
 <textarea id="submit-title" name="submit-title" cols="40" rows="1" onkeydown="disableSubmitButton()"></textarea>
 <span id="title-error" style="color:red">*</span>
 <br>
-Category
+<label for="category">Category</label>
 <select id="category" name="category">
   <option value="fundraising">Fundraising</option>
   <option value="planning">Trek Planning</option>
@@ -26,15 +32,17 @@ Category
 </select>
 <span id="category-error" style="color:red">*</span>
 <br> <br>
-Date
+<label for="date">Date</label>
 <input id="date" type="date" name="date" onkeydown="disableSubmitButton()">
 <span id="date-error" style="color:red">*</span>
 <br> <br>
-Content <br>
+<label for="submit-content">Content </label><br>
 <textarea id="submit-content" name="submit-content" cols="40" rows="5" onkeydown="disableSubmitButton()"></textarea>
 <span id="content-error" style="color:red">*</span>
 <br>
-<br>Image <input type="file" name="files"><br> or video (copy and paste youtube link) <br> <textarea id="video" name="video" cols="40" rows="1" ></textarea> <br> <br>
+<br>
+<label for="image">Image</label>
+<input type="file" name="files"><br> or video (copy and paste youtube link) <br> <textarea id="video" name="video" cols="40" rows="1" ></textarea> <br> <br>
 
 Please save before submitting: <input type="button" id="save-button" value="Save"><br>      
 <INPUT id="submit-button" type="submit" value="Send" disabled="disabled"> <INPUT type="reset">
@@ -43,4 +51,6 @@ Please save before submitting: <input type="button" id="save-button" value="Save
 echo '<script src="submission_page_errors.js"></script>';
 ?>
 <a href="logout.php" style="margin-left:2em">Click here</a> to Logout.
+</html>
+</html>
 
